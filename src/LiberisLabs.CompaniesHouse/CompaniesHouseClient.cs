8ed5b93ae486f6afae3a058f7c597ada533ab822
kev_bite <kev_bite@msn.com>
2015-10-08 15:22:52 +0100

    Adding in CompaniesHouseAuthorizationHandler.

--- src/LiberisLabs.CompaniesHouse/CompaniesHouseClient.cs
+++ src/LiberisLabs.CompaniesHouse/CompaniesHouseClient.cs
@@ -1,6 +1,52 @@
-﻿namespace LiberisLabs.CompaniesHouse
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using LiberisLabs.CompaniesHouse.DelegatingHandlers;
+
+namespace LiberisLabs.CompaniesHouse
 {
+    public interface ICompaniesHouseSettings
+    {
+        Uri BaseUri { get; }
+
+        string ApiKey { get; }
+    }
+
+    public class CompaniesHouseSettings : ICompaniesHouseSettings
+    {
+        public CompaniesHouseSettings(Uri baseUri, string apiKey)
+        {
+            BaseUri = baseUri;
+            ApiKey = apiKey;
+        }   
+
+        public Uri BaseUri { get; }
+
+        public string ApiKey { get; }
+    }
+
     public class CompaniesHouseClient
     {
+        private readonly ICompaniesHouseSettings _settings;
+        
+        public CompaniesHouseClient(ICompaniesHouseSettings settings)
+        {
+            _settings = settings;
+        }
+
+        private HttpClient CreateHttpClient()
+        {
+            var httpClientHandler = new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+
+
+            var httpClient = HttpClientFactory.Create(httpClientHandler, new CompaniesHouseAuthorizationHandler(_settings.ApiKey));
+            
+            httpClient.BaseAddress = _settings.BaseUri;
+
+            return httpClient;
+        }
     }
 }
---
+++ src/LiberisLabs.CompaniesHouse/DelegatingHandlers/CompaniesHouseAuthorizationHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LiberisLabs.CompaniesHouse.DelegatingHandlers
+{
+    public class CompaniesHouseAuthorizationHandler : DelegatingHandler
+    {
+        private readonly string _apiKey;
+
+        public CompaniesHouseAuthorizationHandler(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", Base64Encode(_apiKey));
+
+            return base.SendAsync(request, cancellationToken);
+        }
+
+        private string Base64Encode(string apiKey)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(apiKey));
+        }
+    }
+}
