655ba65af8ffa525530e3dc8cfe0e3ad6cb98b8e
Rui Mourato <rui.mourato@gmail.com>
2016-02-15 18:22:17 +0000

    Client implementation for retrieving company profile details

---
+++ src/LiberisLabs.CompaniesHouse.IntegrationTests/Tests/CompanyProfileTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LiberisLabs.CompaniesHouse.Request;
+using NUnit.Framework;
+
+namespace LiberisLabs.CompaniesHouse.IntegrationTests.Tests
+{
+    [TestFixture]
+    public class CompanyProfileTests
+    {
+        private CompaniesHouseSettings _settings;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            var apiKey = "QMA5Ll3K0YDHwObXHn6rSFDYAqBanaDU2WLU8Do4";//Environment.GetEnvironmentVariable("CompaniesHouseApiKey");
+            _settings = new CompaniesHouseSettings(apiKey);
+        }
+
+        [Test]
+        public void CheckCompanyProfileIsReturned()
+        {
+            const string companyNumber = "03977902"; // Google UK company number, unlikely to go away soon
+            var client = new CompaniesHouseClient(_settings);
+
+            var result = client.GetCompanyProfileAsync(companyNumber).Result;
+
+            Assert.That(result.Data.CompanyName, Is.Not.Empty);
+        }
+    }
+}
---
+++ src/LiberisLabs.CompaniesHouse/UriBuilders/ICompanyProfileUriBuilder.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LiberisLabs.CompaniesHouse.UriBuilders
+{
+    public interface ICompanyProfileUriBuilder
+    {
+        Uri Build(string companyNumber);
+    }
+}
