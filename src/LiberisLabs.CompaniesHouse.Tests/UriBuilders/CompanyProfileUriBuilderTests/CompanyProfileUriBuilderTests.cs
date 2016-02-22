1dfec45649c205cef67900929847aaef08633073
kev_bite <kev_bite@msn.com>
2016-02-22 14:02:57 +0000

    Adding extra test around CompanyProfileUriBuilder

---
+++ src/LiberisLabs.CompaniesHouse.Tests/UriBuilders/CompanyProfileUriBuilderTests/CompanyProfileUriBuilderTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using LiberisLabs.CompaniesHouse.UriBuilders;
+using NUnit.Framework;
+
+namespace LiberisLabs.CompaniesHouse.Tests.UriBuilders.CompanyProfileUriBuilderTests
+{
+    public class CompanyProfileUriBuilderTests
+    {
+        private CompanyProfileUriBuilder _uriBuilder;
+        private Uri _actualUri;
+        private readonly Uri _baseUri = new Uri("http://liberis.co.uk/bla1/bla2/");
+        private string _companyNumber;
+        
+
+        [TestFixtureSetUp]
+        public void GivenACompanyProfileUriBuilder()
+        {
+            _uriBuilder = new CompanyProfileUriBuilder();
+        }
+
+        [SetUp]
+        public void WhenBuildingUriWithCompanySearchRequest()
+        {
+            _companyNumber = "123456789";
+            _actualUri = _uriBuilder.Build(_companyNumber);
+        }
+
+        [Test]
+        public void ThenTheUriIsNotAbsolute()
+        {
+            Assert.That(_actualUri.IsAbsoluteUri, Is.False);
+        }
+
+        [Test]
+        public void ThenTheUriPathIsCorrect()
+        {
+            var uri = new Uri(_baseUri, _actualUri);
+            Assert.That(uri.AbsolutePath, Is.EqualTo("/bla1/bla2/company/" + _companyNumber));
+        }
+   }
+}
