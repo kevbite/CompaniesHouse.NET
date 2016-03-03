242be37073ede3ebbd522d0f353b443af5f6fa0f
Wojciech Turowicz <wojtek.turowicz@gmail.com>
2016-03-03 14:03:21 +0000

    remove unused using

--- src/LiberisLabs.CompaniesHouse/CompaniesHouseCompanyFilingHistoryClient.cs
+++ src/LiberisLabs.CompaniesHouse/CompaniesHouseCompanyFilingHistoryClient.cs
@@ -2,7 +2,6 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using LiberisLabs.CompaniesHouse.Response.CompanyFiling;
-using LiberisLabs.CompaniesHouse.Response.CompanyProfile;
 using LiberisLabs.CompaniesHouse.UriBuilders;
 
 namespace LiberisLabs.CompaniesHouse
