f30639d074533a4e17ca7809fffeee56aeeb7902
sakisk <sakis.koltsidas@gmail.com>
2018-08-08 16:01:16 +0100

    #86 Added missing information from Officer resource

--- src/CompaniesHouse.Tests/ResourceBuilders/Officer.cs
+++ src/CompaniesHouse.Tests/ResourceBuilders/Officer.cs
@@ -1,4 +1,6 @@
 using System;
+using CompaniesHouse.Response;
+using CompaniesHouse.Response.Officers;
 
 namespace CompaniesHouse.Tests.ResourceBuilders
 {
@@ -15,5 +17,15 @@ namespace CompaniesHouse.Tests.ResourceBuilders
         public string OfficerRole { get; set; }
 
         public string Nationality { get; set; }
+
+        public string Occupation { get; set; }
+
+        public Address Address { get; set; }
+
+        public string CountryOfResidence { get; set; }
+
+        public OfficerFormerName[] FormerNames { get; set; }
+
+        public OfficerIdentification Identification { get; set; }
     }
 }
