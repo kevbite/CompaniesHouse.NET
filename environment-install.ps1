264916086c15aa6caf7390e09ab517bf49549895
apolo <agustin.polo@cloudx.com>
2020-02-28 09:53:15 -0300

    remove unused files

--- environment-install.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Checking if pull request"
-
-if ([bool]$env:APPVEYOR_PULL_REQUEST_NUMBER)
-{
-   Write-Host "Build is pull request - updating version"
-   Update-AppveyorBuild -Version "0.0.0-pull-request-$($env:APPVEYOR_PULL_REQUEST_NUMBER)-$($env:APPVEYOR_BUILD_NUMBER)"
-}
