eafb1d04453b859efc5e6f1d6c15cf52c73b255c
Kevin Smith <kev_bite@msn.com>
2020-10-04 23:34:01 +0100

    Including a test to prove what happends when invalid http status code #137.

---
+++ src/CompaniesHouse.Tests/CompaniesHouseSearchClientTests/CompaniesHouseSearchClientTestsForCompanySearchWithTooManyRequests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CompaniesHouse.Request;
+using CompaniesHouse.Response.Search.CompanySearch;
+using CompaniesHouse.UriBuilders;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace CompaniesHouse.Tests.CompaniesHouseSearchClientTests
+{
+    [TestFixture]
+    public class CompaniesHouseSearchClientTestsForCompanySearchWithTooManyRequests
+    {
+        private Exception _caughtException;
+
+        [OneTimeSetUp]
+        public async Task GivenACompanyHouseSearchCompanyClient_WhenSearchingForACompanyAndApiReturnsTooManyRequests()
+        {
+            var uri = new Uri("https://wibble.com/search/companies");
+
+            HttpMessageHandler handler = new TooManyRequestsHttpMessageHandler();
+
+            var uriBuilder = new Mock<ISearchUriBuilder>();
+            uriBuilder.Setup(x => x.Build(It.IsAny<SearchRequest>()))
+                .Returns(uri);
+
+            var uriBuilderFactory = new Mock<ISearchUriBuilderFactory>();
+            uriBuilderFactory.Setup(x => x.Create<CompanySearch>())
+                .Returns(uriBuilder.Object);
+
+            var client = new CompaniesHouseSearchClient(new HttpClient(handler), uriBuilderFactory.Object);
+
+            try
+            {
+                await client.SearchAsync<CompanySearch>(new SearchRequest());
+            }
+            catch (Exception ex)
+            {
+                _caughtException = ex;
+            }
+        }
+
+        [Test]
+        public void ThenExceptionIsThrown()
+        {
+            _caughtException.Should().BeOfType<HttpRequestException>();
+
+            _caughtException.As<HttpRequestException>().Message.Should().StartWith("Response status code does not indicate success: 429");
+        }
+
+    }
+}
