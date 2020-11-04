d06f2d8f7da9b520d9c1d8cb6db84808d515cd0a
HulyaUysal <HulyaUysal@users.noreply.github.com>
2020-11-04 11:37:53 +0000

    .

--- src/CompaniesHouse.Core.IntegrationTests/Keys.cs
+++ src/CompaniesHouse.Core.IntegrationTests/Keys.cs
@@ -4,6 +4,6 @@ namespace CompaniesHouse.Core.IntegrationTests
 {
     public static class Keys
     {
-        public static string ApiKey { get; } = "vJjHSXMBpqIwFlwVfbRfXQtDB1Xl-v5QwwPLfWWU";
+        public static string ApiKey { get; } = "jW_pKnALfDL6DT1o-ZaAWrwTctd4_Uk5qYo5qxKW";
     }
 }
--- src/CompaniesHouse.Core.ScenarioTests/Keys.cs
+++ src/CompaniesHouse.Core.ScenarioTests/Keys.cs
@@ -2,6 +2,6 @@ namespace CompaniesHouse.Core.ScenarioTests
 {
     public static class Keys
     {
-        public static string ApiKey { get; } = "vJjHSXMBpqIwFlwVfbRfXQtDB1Xl-v5QwwPLfWWU";
+        public static string ApiKey { get; } = "jW_pKnALfDL6DT1o-ZaAWrwTctd4_Uk5qYo5qxKW";
     }
 }
