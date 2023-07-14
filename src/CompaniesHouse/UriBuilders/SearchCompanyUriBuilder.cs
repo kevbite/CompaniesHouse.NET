48ad52888dd0adad88c28bdb1dc4848ce88dd9d2
Kevin Smith <kev_bite@msn.com>
2023-07-14 19:58:51 +0100

    Sep files

---
+++ src/CompaniesHouse/UriBuilders/SearchCompanyUriBuilder.cs
@@ -0,0 +1,22 @@
+﻿using CompaniesHouse.Request;
+
+namespace CompaniesHouse.UriBuilders;
+
+public class SearchCompanyUriBuilder : SearchUriBuilder<SearchCompanyRequest>
+{
+    public SearchCompanyUriBuilder(string path) : base(path)
+    {
+    }
+
+    protected override string BuildQuery(SearchCompanyRequest request)
+    {
+        var query = base.BuildQuery(request);
+
+        if (string.IsNullOrWhiteSpace(request.Restrictions))
+        {
+            query += "&restrictions=" + request.Restrictions;
+        }
+
+        return query;
+    }
+}
--- src/CompaniesHouse/UriBuilders/SearchUriBuilder.cs
+++ src/CompaniesHouse/UriBuilders/SearchUriBuilder.cs
@@ -3,25 +3,6 @@ using CompaniesHouse.Request;
 
 namespace CompaniesHouse.UriBuilders
 {
-    public class SearchCompanyUriBuilder : SearchUriBuilder<SearchCompanyRequest>
-    {
-        public SearchCompanyUriBuilder(string path) : base(path)
-        {
-        }
-
-        protected override string BuildQuery(SearchCompanyRequest request)
-        {
-            var query = base.BuildQuery(request);
-
-            if (string.IsNullOrWhiteSpace(request.Restrictions))
-            {
-                query += "&restrictions=" + request.Restrictions;
-            }
-
-            return query;
-        }
-    }
-
     public class SearchUriBuilder<TSearch> : ISearchUriBuilder<TSearch> where TSearch : ISearchRequest
     {
         private readonly string _path;
